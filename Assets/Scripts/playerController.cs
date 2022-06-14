5290cc90abab86d302ae40b4ac8d628b922dab76
equintee <employes23@gmail.com>
2022-06-14 15:23:25 +0300

    Start

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Vector3 cameraOffset;
+    public float speed = 0.35f;
+    public GameObject player;
+    public int level = 1;
+    private void Start()
+    {
+        cameraOffset = Camera.main.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    float _touchBoarder;
+    void Update()
+    {
+        Touch _touch;
+        
+        if (Input.touchCount > 0)
+        {
+            _touch = Input.GetTouch(0);
+            _touchBoarder = player.transform.position.x + _touch.deltaPosition.x * (speed * Time.deltaTime);
+
+            if (_touch.phase == TouchPhase.Moved)
+            {
+                player.transform.position = new Vector3(_touchBoarder, player.transform.position.y, player.transform.position.z);
+
+            }
+
+            if (player.transform.position.x < -4.5f)
+            {
+                player.transform.position = new Vector3(-4.5f, player.transform.position.y, player.transform.position.z);
+            }
+            if (player.transform.position.x > 4.5f)
+            {
+                player.transform.position = new Vector3(4.5f, player.transform.position.y, player.transform.position.z);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/trapController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class trapController : MonoBehaviour
+{
+    private int value;
+    public int operation; //0: +, 1: -, 2: x, 3: /
+
+    [SerializeField]
+    private TextMeshProUGUI _text;
+    void Start()
+    {
+        value = Random.Range(1, 6);
+        operation = Random.Range(0, 4);
+        changeText();
+    }
+
+    private void changeText()
+    {
+        string doorText = "";
+
+        switch (operation)
+        {
+            case 0:
+                doorText += "+";
+                break;
+            case 1:
+                doorText += "-";
+                break;
+            case 2:
+                doorText += "X";
+                break;
+            case 3:
+                doorText += "÷";
+                break;
+        }
+
+        doorText += value.ToString();
+            
+        _text.text = doorText;
+
+       
+    }
+
+}
