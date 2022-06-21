f32097d0d87e6c51a0eb9a54d203654e4af086e5
equintee <employes23@gmail.com>
2022-06-21 16:38:47 +0300

    EU

--- Assets/Scripts/boxingMachineBallRotation.cs
+++ Assets/Scripts/boxingMachineBallRotation.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Threading.Tasks;
 
 public class boxingMachineBallRotation : MonoBehaviour
 {
     [SerializeField]
     private boxingMachineController _boxingMachineController;
 
-    private void OnTriggerEnter(Collider other)
+    public float ballClosingTime;
+    public float shakeTime;
+    public float shakeAngle;
+    private async void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("playerHand")) return;
-        transform.DOLocalRotate(Vector3.zero, 0.5f);
         GetComponent<BoxCollider>().enabled = false;
+        
 
         if(gameController.playerLevel >= _boxingMachineController.value)
         {
-            Debug.Log("zort");
+            transform.DOLocalRotate(new Vector3(90,0,0), ballClosingTime);
+            Camera.main.transform.DOLocalRotate(new Vector3(-7.835f, -180, shakeAngle), shakeTime);
+            await Task.Delay(System.TimeSpan.FromSeconds(shakeTime));
+            Camera.main.transform.DOLocalRotate(new Vector3(-7.835f, -180, -1 * shakeAngle), shakeTime);
+            await Task.Delay(System.TimeSpan.FromSeconds(shakeTime));
+            Camera.main.transform.DOLocalRotate(new Vector3(-7.835f, -180, 0), shakeTime);
+
             int coin = PlayerPrefs.GetInt("coin");
             coin += _boxingMachineController.value;
             PlayerPrefs.SetInt("coin", coin);
         }
         else
         {
-            Debug.Log("sapsal");
             other.transform.parent.gameObject.GetComponent<Animator>().SetTrigger("damage");
+            
         }
 
     }
